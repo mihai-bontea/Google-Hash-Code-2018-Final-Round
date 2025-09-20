9e24b600203a7d673477810f217c4de55e3e3992
mihai-bontea <mihaibontea@protonmail.com>
2025-09-20 12:17:00 +0300

    reading output file

--- validate.py
+++ validate.py
@@ -21,6 +21,21 @@ def read_building(f) -> Building:
     return Building(kind, height, width, res_or_type, walls)
 
 
+@dataclass
+class Solution:
+    buildings_placed: int
+    id_at_coord: List[Tuple[int, Tuple[int, int]]]
+
+def read_solution(filename) -> Solution:
+    with open(filename, 'r') as f:
+        buildings_placed = int(f.readline().strip())
+        id_at_coord = [(project_id, (i, j)) 
+                    for _ in range(buildings_placed)
+                    for project_id, i, j in [map(int, f.readline().strip().split())]]
+    
+    return Solution(buildings_placed, id_at_coord)
+
+
 solutions = ["sol1"]
 input_files = ["a_example", "b_short_walk", "c_going_green", "d_wide_selection", "e_precise_fit", "f_different_footprints"]
 
@@ -28,3 +43,11 @@ for input_file in input_files:
     with open(f"input_files/{input_file}.in", 'r') as f:
         city_height, city_width, max_walking_dist, nr_building_projects = list(map(int, f.readline().split()))
         buildings = [read_building(f) for _ in range(nr_building_projects)]
+
+        for solution in solutions:
+            output_file = f"output_files/{solution}/{input_file}.out"
+            try:
+                solution = read_solution(output_file)
+
+            except FileNotFoundError:
+                print(f"Error: File '{output_file}' not found.")
